c0c51f60c0578d80e7c5aeae242a3fdb02c4b1eb
Lairton <130435095+Laiirton@users.noreply.github.com>
2026-03-24 00:24:03 -0300

    Create main.go

---
+++ cmd/api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/laiirton/solucoes-urbanas-api/internal/config"
+	"github.com/laiirton/solucoes-urbanas-api/internal/database"
+	"github.com/laiirton/solucoes-urbanas-api/internal/repository"
+	"github.com/laiirton/solucoes-urbanas-api/internal/routes"
+)
+
+func main() {
+	cfg := config.Load()
+
+	if cfg.DatabaseURL == "" {
+		log.Fatal("DATABASE_URL environment variable is required")
+	}
+	if cfg.JWTSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is required")
+	}
+
+	db, err := database.Connect(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.RunMigrations(cfg.DatabaseURL); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+
+	userRepo := repository.NewUserRepository(db.Pool)
+	router := routes.Setup(userRepo, cfg.JWTSecret)
+
+	addr := fmt.Sprintf(":%s", cfg.Port)
+	log.Printf("Server starting on %s", addr)
+
+	if err := http.ListenAndServe(addr, router); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
