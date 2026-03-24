042c24d4040f6aacd564021539a9bb40335b6de8
Lairton <130435095+Laiirton@users.noreply.github.com>
2026-03-24 00:23:51 -0300

    Create database.go

---
+++ internal/database/database.go
@@ -0,0 +1,58 @@
+package database
+
+import (
+	"context"
+	"embed"
+	"fmt"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
+
+type DB struct {
+	Pool *pgxpool.Pool
+}
+
+func Connect(databaseURL string) (*DB, error) {
+	pool, err := pgxpool.New(context.Background(), databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
+	}
+
+	if err := pool.Ping(context.Background()); err != nil {
+		return nil, fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	log.Println("Connected to database successfully")
+	return &DB{Pool: pool}, nil
+}
+
+func (db *DB) RunMigrations(databaseURL string) error {
+	src, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		return fmt.Errorf("failed to create migration source: %w", err)
+	}
+
+	m, err := migrate.NewWithSourceInstance("iofs", src, databaseURL)
+	if err != nil {
+		return fmt.Errorf("failed to create migrator: %w", err)
+	}
+	defer m.Close()
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to run migrations: %w", err)
+	}
+
+	log.Println("Database migrations applied successfully")
+	return nil
+}
+
+func (db *DB) Close() {
+	db.Pool.Close()
+}
