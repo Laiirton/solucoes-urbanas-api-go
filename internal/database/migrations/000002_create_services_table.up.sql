e7cc4ce0da381d483515fb0cde1323e068045d34
Lairton <130435095+Laiirton@users.noreply.github.com>
2026-04-06 18:54:48 -0300

    Delete 000002_create_services_table.up.sql

--- internal/database/migrations/000002_create_services_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS services (
-    id          BIGSERIAL PRIMARY KEY,
-    title       VARCHAR   NOT NULL,
-    description TEXT,
-    category    VARCHAR   NOT NULL,
-    is_active   BOOLEAN   NOT NULL DEFAULT TRUE,
-    created_at  TIMESTAMPTZ DEFAULT NOW(),
-    updated_at  TIMESTAMPTZ DEFAULT NOW()
-);
